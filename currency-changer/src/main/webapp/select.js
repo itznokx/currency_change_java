a2462706f87a6337a9c5e9ddcbb4b7db4e825c44
itznokx <dimitri.b.medeiros@gmail.com>
2024-05-20 20:10:13 -0300

    Exclude js file

--- currency-changer/src/main/webapp/select.js
+++
@@ -1,4 +0,0 @@
-const brl = {name:"BRL - BRAZIL",language:"pt",country:"BR"};
-const usd = {name:"USD - USA",language:"us",country:"US"}
-var selectList = document.getElementById("inputCurrency");
-var selectToCurrencyList = document.getElementById("inputToCurrency")
