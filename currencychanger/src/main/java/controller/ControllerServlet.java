82cbe412900eb2f729ffaefa2809c0be12a6eb1c
itznokx <dimitri.b.medeiros@gmail.com>
2024-06-19 16:44:39 -0300

    Fix doPost function

--- currencychanger/src/main/java/controller/ControllerServlet.java
+++ currencychanger/src/main/java/controller/ControllerServlet.java
@@ -1,13 +1,12 @@
 package controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import model.CurrencyHeader;
 
 public class ControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -22,20 +21,12 @@ public class ControllerServlet extends HttpServlet {
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 *      response)	
 	 */
-	protected void service(HttpServletRequest request, 
-						   HttpServletResponse response) 
-	throws ServletException, IOException{
-		String urlServlet = request.getServletPath();
-		request.setAttribute("currencyList", CurrencyHeader.getCurrencies());
-		request.setAttribute("currencyToList", CurrencyHeader.getCurrencies());
-		request.getRequestDispatcher("index.jsp").forward(request, response);
-	}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-
+		doPost(request,response);
 	}
 
 	/**
@@ -45,7 +36,8 @@ public class ControllerServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setAttribute("result", request.getParameter("selectCurrency"));
+		request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
 }
